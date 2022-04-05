6bb76ca5ff43fd0791d3777a5175e83bea6b72ac
Eric Ballard <ericballard7@gmail.com>
2022-04-05 16:28:26 -0400

    0.1.0 ~ port_scanner   + multi-threading   + stats   + cli

--- src/clients/tools/port_scanner.py
+++ src/clients/tools/port_scanner.py
@@ -1,53 +1,132 @@
+# Exmple Half-Open scan, other viable types of stealth scanning; XMas Tree, FIN, Null
+
+# NOTE: Windows
+# Microsoft operating systems in addition to a number of others have
+# ignored the RFC for TCP and have implemented it somewhat differently than the
+# standard. This causes the FIN, Xmas tree, and Null scans to fail on Windows based
+# operating systems, and others such as Cisco, HP/UX, and IRIX.
+
+# https://www.giac.org/paper/gsec/1985/stealth-port-scanning-methods/103446
+
+from socket import getservbyport
 from scapy.all import *
-import socket
+import threading
+import math
+import time
+import sys
 
-# Silence scapy debug
-conf.verb = 0
-
-ip = "107.152.41.214"
-port = int(22)
+# Add sibling folder to path, import util modules
+sys.path.append(os.path.abspath("./src/utils"))
+import cli
 
 
-def is_online():
-    reply = sr1(IP(dst=ip, ttl=20) / ICMP(), timeout=2)
+def is_online(host):
+    reply = sr1(IP(dst=host, ttl=20) / ICMP(), timeout=2, verbose=0)
     return False if reply is None else True
 
 
-def is_port_open(port):
+def is_port_open(host, port):
     # Send SYN packet
-    response = sr1(IP(dst=ip) / TCP(dport=port, flags="S"), timeout=2, verbose=0)
+    response = sr1(IP(dst=host) / TCP(dport=port, flags="S"), timeout=2, verbose=0)
 
     try:
-        # Check for response, if available check for ACK
+        # Check for response, if available check for SYN-ACK flag
+        # Any other response the port is closed
         if response.getlayer(TCP).flags == "SA":
+
+            # Acknowledge server response, terminate connection
+            # (Releases port, otherwise this would be a SYN flood attack)
+            sr1(IP(dst=host) / TCP(dport=port, flags="R"), timeout=2, verbose=0)
             return True
     except AttributeError:
         pass
     return False
 
 
+def scan_range(host, fro, to):
+    global scanned_ports
+
+    # NOTE: range(1-100) Will start @ index 1 and end @ index 99 - ??
+    for i in range(fro, to):
+        scanned_ports += 1
+
+        if is_port_open(host, i):
+            # Print open ports with related service
+            print("OPEN: %s => %s" % (i, getservbyport(i, "TCP")))
+
+
 # Main()
+scanned_ports = 0
+
 if __name__ == "__main__":
-    print("Checking if host is online...")
+    # Parse CLI args
+    args = cli.get_args()
+
+    # Init config
+    start_port = int(args.start)
+    end_port = int(args.end)
+    total_ports = end_port - start_port
+
+    # Validate input
+    if total_ports < 1:
+        print(f"@FAILED - Invalid Port Range: (Start: {start_port} End: {end_port})")
+        sys.exit(1)
+
+    start_time = time.time()
+    print("@CHECKING if host is online...")
 
     # Check if host is online
     for i in range(5):
-        online = is_online()
+        online = is_online(args.ip)
 
         if online:
             break
 
     if online:
-        print("Scanning ports...")
-        open_ports = []
-
         # Scan ports, cache index if open
-        for i in range(1, 100):
-            if is_port_open(i):
-                open_ports.append(i)
+        print(f"@SCANNING ports... ({start_port} - {end_port}) | THREADS: {args.threads}")
 
-        # Print open ports with related service
-        for port in open_ports:
-            print ("OPEN: %s => %s" %(port, socket.getservbyport(port, "TCP"))) 
+        try:
+            # Multi-Threaded scanner (Default 1)
+            spawned_threads = []
+            threads = int(args.threads)
+
+            # Calculate # of ports to scan for each thread
+            excess_load = total_ports % threads
+            ports_per_thread = math.floor(total_ports / threads)
+
+            # Start em up
+            for t in range(threads):
+                # Apply desired load for thread
+                thread_load = (
+                    ports_per_thread + excess_load
+                    if t == threads - 1
+                    else ports_per_thread
+                )
+
+                # Apply range of ports for thread
+                thread_start_port = start_port if t == 0 else thread_end_port
+                thread_end_port = thread_start_port + thread_load
+
+                # SManage child thread
+                child_thread = threading.Thread(
+                    target=scan_range,
+                    args=(args.ip, thread_start_port, thread_end_port),
+                )
+
+                spawned_threads.append(child_thread)
+                child_thread.start()
+
+            # Wait for child threads to finish
+            for thread in spawned_threads:
+                thread.join()
+
+        except KeyboardInterrupt:
+            # User interrupted
+            print("@INTERRUPTED...")
+
+        # Print stats
+        run_time = time.time() - start_time
+        print(f"Scanned {scanned_ports} ports in {round(run_time, 2)}s | {args.ip}")
     else:
-        print(f"Unable to ping host, is it online? ({ip})")
+        print(f"@FAILED to ping host, is it online? | {args.ip}")
--- src/utils/cli.py
+++ src/utils/cli.py
@@ -4,6 +4,10 @@ def get_args():
     # Check for CLI args / Set defaults
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip", help="Target address to bind", default="127.0.0.1")
-    parser.add_argument("--port", help="Target port to bind", default="25565")
+    parser.add_argument("--port", help="Target port to bind", default=80)
     parser.add_argument("--tcp", help="Set server to use TCP", default=False)
+    
+    parser.add_argument("--threads", help="Set # of threads for scanner", default=1)
+    parser.add_argument("--start", help="Set start port for scanner", default=1)
+    parser.add_argument("--end", help="Set end port for scanner", default=100)
     return parser.parse_args()
